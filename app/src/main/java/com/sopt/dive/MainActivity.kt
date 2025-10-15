d2f1d0f901b91a9c5d0a8daf76f95787d6958579
SHC <hcshin0717@naver.com>
2025-10-16 02:08:22 +0900

    feat: week1 - MainActivity 이미지 추가

--- app/src/main/java/com/sopt/dive/MainActivity.kt
+++ app/src/main/java/com/sopt/dive/MainActivity.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,10 +70,15 @@ fun MainScreen(
         Column {
             Row {
                 Surface(
-                    color = Color.Blue,
+                    shape = RoundedCornerShape(100),
+                    color = Color.Unspecified,
                     modifier = Modifier
-                        .size(20.dp),
+                        .size(40.dp),
                 ) {
+                    Image(
+                        painter = painterResource(R.drawable.img_otter),
+                        contentDescription = "달수",
+                    )
                 }
                 Text(
                     text = userName
