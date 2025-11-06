a3996100d292cb5b5bdb1fbe6d75010c5c75ef8f
SHC <hcshin0717@naver.com>
2025-11-07 01:58:53 +0900

    feat: week3 - coil 사용을 위한 dependencies 추가 및 kotlin 버전 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
