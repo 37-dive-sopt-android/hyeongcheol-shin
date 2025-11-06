8fd65ea037cc43bac257230f74bba99a10f7b82d
SHC <hcshin0717@naver.com>
2025-11-06 17:20:42 +0900

    feat: week2 심화 - DummyData 수정

--- app/src/main/java/com/sopt/dive/data/UserDataModel.kt
+++ app/src/main/java/com/sopt/dive/data/UserDataModel.kt
@@ -17,14 +17,14 @@ data class UserData(
 fun getUserDummyData(): List<UserData> {
     return listOf(
         UserData("SHC", "Fe"),
-        UserData("SHC", "Fe", image = R.drawable.img_lion, comment = "Test"),
-        UserData("SHC", "Fe", birth = LocalDate.now()),
+        UserData("Lion", "동물의 왕국", image = R.drawable.img_lion, comment = "사자로 만든 탕"),
+        UserData("Coffee", "코피", birth = LocalDate.now()),
         //생일 확인용 더미 데이터
-        UserData("SHC", "Fe", image = R.drawable.img_fox, comment = "TestTestTestTestTestTestTestTestTestTestTestTest"),
-        UserData("SHC", "Fe", favoriteBoardGame = BoardGame("Test", "Test", Difficulty.EASY)),
-        UserData("SHC", "Fe", image = R.drawable.img_otter, imageDescription = "test"),
-        UserData("SHC", "Fe", image = R.drawable.img_duck, comment = "TestTestTestTestTestTestTestTestTest", favoriteBoardGame = BoardGame("Test", "Test", Difficulty.NORMAL)),
-        UserData("SHC", "Fe", comment = "TestTestTestTestTestTestTestTestTest", favoriteBoardGame = BoardGame(title = "Test", difficulty = Difficulty.HARD)),
-        UserData("SHC", "Fe", birth = LocalDate.now(), comment = "TestTestTestTestTestTestTestTestTest", favoriteBoardGame = BoardGame(title = "Test", description = "Test", difficulty = Difficulty.HARD)),
+        UserData("Fox", "여우는 멍멍", image = R.drawable.img_fox, comment = "여우는 여우여우하고 우나여우"),
+        UserData("누룽지", "바비 브라운", favoriteBoardGame = BoardGame("Bang", "서부의 무법자", Difficulty.EASY)),
+        UserData("달수", "보노보노", image = R.drawable.img_otter, imageDescription = "otter"),
+        UserData("바나나", "바나나킥", image = R.drawable.img_duck, comment = "바나나가 어디를 보고 바나나 방금 웃었죠? 바나나킥", favoriteBoardGame = BoardGame("테라포밍마스", "화성개척", Difficulty.NORMAL)),
+        UserData("화상전화", "Fire", comment = "세상에서 가장 뜨겁고도 뜨거운 전화", favoriteBoardGame = BoardGame(title = "할리갈리", difficulty = Difficulty.HARD)),
+        UserData("우왕좌왕", "KingKing", birth = LocalDate.now(), comment = "오른쪽에도 왕이 있고 왼쪽에도 왕이 있으니 온 사방이 왕이네", favoriteBoardGame = BoardGame(title = "젠가", description = "피지컬 게임", difficulty = Difficulty.HARD)),
     )
 }
