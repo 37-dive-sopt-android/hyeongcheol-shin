11ad76a48c969ad0c50233953e07e584c9904690
SHC <hcshin0717@naver.com>
2025-11-07 23:19:00 +0900

    feat: week3 - ViewModel을 이용해 myData와 UserData 관리

--- app/src/main/java/com/sopt/dive/navigation/NavManager.kt
+++ app/src/main/java/com/sopt/dive/navigation/NavManager.kt
@@ -1,15 +1,19 @@
 package com.sopt.dive.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.sopt.dive.data.User
-import com.sopt.dive.data.getUserDummyData
 import com.sopt.dive.ui.main.home.HomeScreen
+import com.sopt.dive.ui.main.home.HomeViewModel
 import com.sopt.dive.ui.main.mypage.MyPageScreen
 import com.sopt.dive.ui.main.search.SearchScreen
 import com.sopt.dive.ui.signin.SignInScreen
@@ -30,6 +34,8 @@ fun NavigationMainScreen(
     modifier: Modifier,
     startDestination: String = Screen.Root.name,
 ) {
+    var homeViewModel: HomeViewModel? = null
+
     NavHost(
         navController = navController,
         startDestination = startDestination
@@ -41,7 +47,10 @@ fun NavigationMainScreen(
             fun appHandle(): SavedStateHandle =
                 navController.getBackStackEntry(Screen.Root.name).savedStateHandle
 
-            composable(Screen.SignIn.name) {
+            composable(Screen.SignIn.name) { backStackEntry ->
+                val rootEntry = remember(backStackEntry) { navController.getBackStackEntry(Screen.Root.name) }
+                val homeViewModel: HomeViewModel = homeViewModel ?: viewModel(rootEntry)
+
                 val registeredUserId = appHandle().get<String>("user_id").orEmpty()
                 val registeredUserPw = appHandle().get<String>("user_pw").orEmpty()
 
@@ -50,6 +59,15 @@ fun NavigationMainScreen(
                     registeredUserPw = registeredUserPw,
                     onSignUpClick = { navController.navigate(Screen.SignUp.name) },
                     onSignInClick = {
+                        homeViewModel.setMyProfile(
+                            User(
+                                id = appHandle().get<String>("user_id").orEmpty(),
+                                pw = appHandle().get<String>("user_pw").orEmpty(),
+                                nickname = appHandle().get<String>("user_nickname").orEmpty(),
+                                drinking = appHandle().get<String>("user_drinking").orEmpty(),
+                                name = appHandle().get<String>("user_name").orEmpty(),
+                            )
+                        )
                         navController.navigate(Screen.Home.name) {
                             popUpTo(Screen.SignIn.name) {
                                 inclusive = true
@@ -74,13 +92,20 @@ fun NavigationMainScreen(
                 )
             }
 
-            composable(Screen.Home.name) {
-                val users = getUserDummyData() + getUserDummyData()
+            composable(Screen.Home.name) { backStackEntry ->
+                val rootEntry = remember(backStackEntry) { navController.getBackStackEntry(Screen.Root.name) }
+                val homeViewModel: HomeViewModel = homeViewModel ?: viewModel(rootEntry)
+
+                val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+
+                if(uiState.userDataList.isEmpty()){
+                    homeViewModel.setDummyUserDataList()
+                }
 
                 HomeScreen(
-                    userName = appHandle().get<String>("user_name").orEmpty(),
-                    userNickname = appHandle().get<String>("user_nickname").orEmpty(),
-                    users = users,
+                    userName = uiState.myData?.name ?: "",
+                    userNickname = uiState.myData?.nickname ?: "",
+                    users = homeViewModel.getUserList(),
                     modifier = modifier,
                 )
             }
@@ -89,15 +114,12 @@ fun NavigationMainScreen(
                     modifier = modifier,
                 )
             }
-            composable(Screen.MyPage.name) {
+            composable(Screen.MyPage.name) { backStackEntry ->
+                val rootEntry = remember(backStackEntry) { navController.getBackStackEntry(Screen.Root.name) }
+                val homeViewModel: HomeViewModel = homeViewModel ?: viewModel(rootEntry)
+
                 MyPageScreen(
-                    user = User(
-                        id = appHandle().get<String>("user_id").orEmpty(),
-                        pw = appHandle().get<String>("user_pw").orEmpty(),
-                        nickname = appHandle().get<String>("user_nickname").orEmpty(),
-                        drinking = appHandle().get<String>("user_drinking").orEmpty(),
-                        name = appHandle().get<String>("user_name").orEmpty(),
-                    ),
+                    user = homeViewModel.getMyProfile(),
                     modifier = modifier,
                 )
             }
