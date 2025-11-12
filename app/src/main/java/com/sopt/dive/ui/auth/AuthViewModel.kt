3cdca2f91ed1f80580f7104e319dceaec3ed549e
SHC <hcshin0717@naver.com>
2025-11-13 03:11:42 +0900

    refactor: week3 - unused import 제거

--- app/src/main/java/com/sopt/dive/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/sopt/dive/ui/auth/AuthViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.dive.data.User
 import com.sopt.dive.data.dataStore.MyProfileRepository
-import com.sopt.dive.ui.main.home.HomeUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
