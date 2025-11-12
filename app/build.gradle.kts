995b8df9d6e01fb43b33ef0e0e39a1f85aed4c4b
SHC <hcshin0717@naver.com>
2025-11-13 02:58:33 +0900

    fix: week3 - jvmTarget 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
