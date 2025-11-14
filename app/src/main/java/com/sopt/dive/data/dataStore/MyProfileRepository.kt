e426666fa765551fa9c849d998cd0ba985a717c0
SHC <hcshin0717@naver.com>
2025-11-14 23:09:20 +0900

    refactor: week4 - DataStore 및 Repository 관련 값 수정

--- app/src/main/java/com/sopt/dive/data/dataStore/MyProfileRepository.kt
+++ app/src/main/java/com/sopt/dive/data/dataStore/MyProfileRepository.kt
@@ -14,14 +14,18 @@ class MyProfileRepository(private val context: Context) {
         prefs[keys.IS_SIGNED_IN] ?: false
     }
 
+    fun getUserId(): Flow<Int> = dataStore.data.map { prefs ->
+        prefs[keys.USER_ID] ?: 0
+    }
+
     fun getMyProfile(): Flow<User> = dataStore.data.map { prefs ->
-        val id = prefs[keys.USER_ID] ?: ""
+        val name = prefs[keys.USER_NAME] ?: ""
         val pw = prefs[keys.USER_PW] ?: ""
         val nickname = prefs[keys.USER_NICKNAME] ?: ""
-        val drinking = prefs[keys.USER_DRINKING] ?: ""
-        val name = prefs[keys.USER_NAME] ?: ""
+        val email = prefs[keys.USER_EMAIL] ?: ""
+        val age = prefs[keys.USER_AGE] ?: 0
 
-        User(id, pw, nickname, drinking, name)
+        User(name, pw, nickname, email, age)
     }
 
     suspend fun setSignInStatus(isSignedIn: Boolean) {
@@ -30,13 +34,19 @@ class MyProfileRepository(private val context: Context) {
         }
     }
 
+    suspend fun saveMyId(userId: Int) {
+        dataStore.edit { prefs ->
+            prefs[keys.USER_ID] = userId
+        }
+    }
+
     suspend fun saveMyProfile(user: User) {
         dataStore.edit { prefs ->
-            prefs[keys.USER_ID] = user.id
+            prefs[keys.USER_NAME] = user.name
             prefs[keys.USER_PW] = user.pw
             prefs[keys.USER_NICKNAME] = user.nickname
-            prefs[keys.USER_DRINKING] = user.drinking
-            prefs[keys.USER_NAME] = user.name
+            prefs[keys.USER_EMAIL] = user.email
+            prefs[keys.USER_AGE] = user.age
         }
     }
 }
