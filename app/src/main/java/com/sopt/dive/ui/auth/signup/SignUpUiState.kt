ee44c67f5b80d1a0c7fcc394db8ded78266842bc
SHC <hcshin0717@naver.com>
2025-11-14 23:45:49 +0900

    refactor: week4 - SignUpUiState 더미 값 제거

--- app/src/main/java/com/sopt/dive/ui/auth/signup/SignUpUiState.kt
+++ app/src/main/java/com/sopt/dive/ui/auth/signup/SignUpUiState.kt
@@ -1,10 +1,10 @@
 package com.sopt.dive.ui.auth.signup
 
 data class SignUpUiState(
-    var inputUserName: String = "asdasd",
-    var inputUserPw: String = "asdasdasd",
-    var inputUserNickname: String = "Fe",
-    var inputUserEmail: String = "hcshin0717@naver.com",
-    var inputUserAge: Int = 25,
+    var inputUserName: String = "",
+    var inputUserPw: String = "",
+    var inputUserNickname: String = "",
+    var inputUserEmail: String = "",
+    var inputUserAge: Int = 0,
     val isLoading: Boolean = false
 )
